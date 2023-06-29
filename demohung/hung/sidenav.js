cc8f9265849bd6610463923f7e271d0bf018775b
hung.nguyentuan <hungg1004@gmail.com>
2023-06-29 17:20:38 +0700

    hung commit

--- demohung/hung/sidenav.js
+++ demohung/hung/sidenav.js
@@ -2,10 +2,20 @@ window.addEventListener("click", (event) => {
   handleClickOutsideNav(event);
 });
 
-function openNav() {
+function openLeftNav() {
   const overlayEle = document.querySelector(".overlay");
 
-  document.getElementById("mySidenav").style.width = "400px";
+  document.getElementById("leftSidenav").style.width = "400px";
+  overlayEle.classList.toggle("active");
+  setTimeout(() => {
+    overlayEle.dataset.status = "active";
+  }, 200);
+}
+
+function openRightNav() {
+  const overlayEle = document.querySelector(".overlay");
+
+  document.getElementById("rightSidenav").style.width = "400px";
   overlayEle.classList.toggle("active");
   setTimeout(() => {
     overlayEle.dataset.status = "active";
@@ -16,16 +26,25 @@ function closeNav() {
   const overlayEle = document.querySelector(".overlay");
 
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("rightSidenav").style.width = "0";
   overlayEle.classList.toggle("active");
   overlayEle.removeAttribute("data-status");
 }
 
 function handleClickOutsideNav(event) {
   const sideNav = document.querySelector("#mySidenav");
+  const sideNav2 = document.querySelector("#rightSidenav");
   const overlay = document.querySelector(".overlay");
 
   if (
     !sideNav.contains(event.target) &&
+    overlay.classList.contains("active") &&
+    overlay.dataset.status === "active"
+  ) {
+    closeNav();
+  }
+  if (
+    !sideNav2.contains(event.target) &&
     overlay.classList.contains("active") &&
     overlay.dataset.status === "active"
   ) {
